49149b426bd40b957e3b8fee134e9c3f88ddfc29
Andrevalqui <luiggi.valqui@tecsup.edu.pe>
2026-01-10 19:30:57 -0500

    Update web.py

--- web.py
+++ web.py
@@ -11,16 +11,16 @@ CASA_INFO = {
     "descripcion": "Un refugio exclusivo frente al mar donde el diseño moderno se encuentra con la naturaleza. Ideal para desconectar y recargar energías.",
     "precio": "Consultar Vía WhatsApp",
     "whatsapp": "987654321",
-    # Tu enlace largo de Google Maps:
     "mapa_link": "https://www.google.com/maps/dir/Plaza+de+Armas+de+Pacasmayo,+Calle+Manco+C%C3%A1pac,+Pacasmayo/-7.4553016,-79.5761959/@-7.4238644,-79.5677181,14z/data=!4m9!4m8!1m5!1m1!1s0x904d46080fc459e5:0xfb2f4f890f3a7d06!2m2!1d-79.5722674!2d-7.40112!1m0!3e0?entry=ttu&g_ep=EgoyMDI2MDEwNy4wIKXMDSoASAFQAw%3D%3D"
 }
 
-# --- LISTA DE SERVICIOS ACTUALIZADA (NUEVOS ÍCONOS Y TEXTOS) ---
+# --- LISTA DE SERVICIOS ACTUALIZADA ---
 SERVICIOS = [
     {"icono": "fa-utensils", "nombre": "Área de Comedor"},
     {"icono": "fa-music", "nombre": "Área de Baile"},
     {"icono": "fa-water", "nombre": "Piscina Privada"},
     {"icono": "fa-fire", "nombre": "Zona de Parrilla"},
+    {"icono": "fa-tree", "nombre": "Áreas Verdes"},
     {"icono": "fa-bed", "nombre": "1 Habitación"},
     {"icono": "fa-bath", "nombre": "2 Baños"},
     {"icono": "fa-car", "nombre": "Estacionamiento Seguro"},
@@ -58,4 +58,3 @@ def home():
 
 if __name__ == '__main__':
     app.run()
-
